14c28a80a421f0eb40f1c8bed19ebc9238681170
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2024-11-24 07:11:01 +0000

    Maths and Numbers

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,21 @@
+// const num = 100;
+// console.log(num);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,5,6,8));
+// console.log(Math.max(4,3,5,6,8));
+
+console.log(Math.floor((Math.random()*100)) + 1);
+const min =10
+const max=20
+console.log(Math.floor(Math.random()* (max-min+1))+min);
+// line 20 says that if generate random number btw min and max values
