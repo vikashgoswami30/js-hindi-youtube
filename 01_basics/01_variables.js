b90e74ec75863ba664ea1dfdbfcdcf3f2bf22d55
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-08 07:17:21 +0000

    data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,33 @@
-const accountID = 144553
-let accountEmail = "Vikash@google.com"
-var accountPassword = "123456"
+const accountId = 144553
+let accountEmail = "vikash@gmaik.com"
+var accountPassword = "12334"
+accountCity = "Dharuhera"
 
-accountCity="Jaipur"
+// accountId = 2 // not allowed to assigned in constants
+accountEmail = "goswami@gmail.com"
+accountPassword = "4321"
+accountCity = "Jaipur"
 
-// accountID=2;
-accountEmail="Bikku@gmail.com"
-accountPassword="Bikku Bhai"
-accountCity="Delhi"
-console.log(accountID);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+// console.log(accountId);
 
-console.table([accountEmail,accountID,accountPassword,accountCity])
+console.table([accountEmail, accountPassword, accountPassword, accountCity]);
+
+// alert(3+3) we are using nodejs not browser
+
+let name  = "Vikash"
+let age = 24
+let isloggoedin = true
+
+// number => 2 to power 53
+// bigint
+// string=> "prefer double quote"
+// boolean => "either true or false"
+// null => standalone value
+// unfedefined => 
+// symbol => euniqueness if using react
+// object =>
+    console.log(typeof undefined);
+    
