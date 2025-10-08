f60b186fb61dd377ce49d35a4e178e0949a0b4bb
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-08 15:46:41 +0000

    conversion learn

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,19 @@
 let score ="33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber= Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 //"33" => 33 
 // "true" => 1 ,false =0
 // "33abc" => NaN-> Not a number and type of is number
 
-let isLoggedIn= "Vikash";
-let booleanIsLoggedIn= Boolean(isLoggedIn);
-console.log( booleanIsLoggedIn);
+// let isLoggedIn= "Vikash";
+// let booleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log( booleanIsLoggedIn);
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
