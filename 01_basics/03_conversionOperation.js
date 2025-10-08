033760db7c72ac4d2e19ff132f2c00465b2470ef
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-08 17:27:00 +0000

    operator and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,19 +1,67 @@
-let score ="33"
+// // // let score ="33"
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// // // // console.log(typeof score);
+// // // // console.log(typeof(score));
 
-let valueInNumber= Number(score);
-// console.log(typeof valueInNumber);
+// // // let valueInNumber= Number(score);
+// // // // console.log(typeof valueInNumber);
 
-//"33" => 33 
-// "true" => 1 ,false =0
-// "33abc" => NaN-> Not a number and type of is number
+// // //"33" => 33 
+// // // "true" => 1 ,false =0
+// // // "33abc" => NaN-> Not a number and type of is number
 
-// let isLoggedIn= "Vikash";
-// let booleanIsLoggedIn= Boolean(isLoggedIn);
-// console.log( booleanIsLoggedIn);
+// // // let isLoggedIn= "Vikash";
+// // // let booleanIsLoggedIn= Boolean(isLoggedIn);
+// // // console.log( booleanIsLoggedIn);
 
-let someNumber = 33 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// // // let someNumber = 33 
+// // // let stringNumber = String(someNumber)
+// // // // console.log(typeof stringNumber);
+
+// // // /* Operations on data types*/
+
+// // // let value =3
+// // // let negValue = -value
+
+// // // console.log(2+2);
+// // // console.log(2-2);
+// // // console.log(2*3);
+// // // console.log(2**3);
+// // // console.log(2/3);
+
+// // // let str1 = "hello"
+// // // let str2 = " vikash"
+// // // let str3 = str1 + str2
+// // // console.log(str3);
+
+// // // console.log("1" + 2);
+// // // console.log(1 + "2");
+// // // console.log("1" + 2 + 2);
+// // // console.log(1 + 2 + "2");
+
+// // // console.log(+"");
+// // // let gameCounter = 100
+// // // gameCounter++
+// // // console.log(gameCounter);
+
+// // console.log(2<4);
+// // console.log(6>=4);
+// // console.log(2==4);
+// // console.log(4===4);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+let userOne={
+    email:"Vikash@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email="vikashgoswami3010@gmail.com"
+console.log(userOne);
+
+
