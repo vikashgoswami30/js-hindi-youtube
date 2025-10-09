8cb258edada08c1f09b8b6fb0762e5c70c4a6a9f
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-09 06:59:03 +0000

    Maths amd javascript basics

--- 01_basics/nums_maths.js
+++ 01_basics/nums_maths.js
@@ -1,21 +1,35 @@
-// const num = 100;
-// console.log(num);
+// // const score = 400
 
-// const balance = new Number(100);
-// console.log(balance);
+// // const balance = new Number(100)
+// // console.log(typeof score);
 
+// // console.log(typeof balance);
 
-// console.log(balance.toString());
+// // console.log(balance.toString().length);
+// // console.log(balance.toFixed(2));
+
+// // const otherNumber = 1129.996
+// // console.log(otherNumber.toPrecision(3));
+
+// // const hundreds = 10000000
+// // console.log(hundreds.toLocaleString('en-IN'));
+
+// // +++++++++++++ MATHS ++++++++++++++++++++
+
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.1));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4,3,5,6,8));
-// console.log(Math.max(4,3,5,6,8));
 
-console.log(Math.floor((Math.random()*100)) + 1);
-const min =10
-const max=20
-console.log(Math.floor(Math.random()* (max-min+1))+min);
-// line 20 says that if generate random number btw min and max values
+// console.log(Math.abs(-5)); // only convert negative value into positive
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(3,4,1,5,8));
+// console.log(Math.max(3,4,1,5,8));
+
+
+console.log(Math.random()*10 + 1);
+
+const min =1
+const max =6
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min );
+
