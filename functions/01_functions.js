8325e52b15384d55cd445778cbff6089d745d27a
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-11 19:47:44 +0000

    handle objects in functions

--- functions/01_functions.js
+++ functions/01_functions.js
@@ -1,36 +1,57 @@
-// // // const checkType1 = 3 + "4"
-// // // const checkType2 = "3" + 4
-// // // const checkType3 = "3" + "4"
-// // // const checkType4 = 3 + 4
+// // // // const checkType1 = 3 + "4"
+// // // // const checkType2 = "3" + 4
+// // // // const checkType3 = "3" + "4"
+// // // // const checkType4 = 3 + 4
 
-// // // const checkType5= 3 + "a"
-// // // const checkType6 = "a" + 4
-// // // const checkType7 = "3" + "4"
-// // // const checkType8 = "3a" + 4
+// // // // const checkType5= 3 + "a"
+// // // // const checkType6 = "a" + 4
+// // // // const checkType7 = "3" + "4"
+// // // // const checkType8 = "3a" + 4
 
-// // // console.log(typeof checkType1);
-// // // console.log(typeof checkType2);
-// // // console.log(typeof checkType3);
-// // // console.log(typeof checkType4);
-// // // console.log(typeof checkType5);
-// // // console.log(typeof checkType6);
-// // // console.log(typeof checkType7);
-// // // console.log(typeof checkType8);
+// // // // console.log(typeof checkType1);
+// // // // console.log(typeof checkType2);
+// // // // console.log(typeof checkType3);
+// // // // console.log(typeof checkType4);
+// // // // console.log(typeof checkType5);
+// // // // console.log(typeof checkType6);
+// // // // console.log(typeof checkType7);
+// // // // console.log(typeof checkType8);
 
 
-// // function addTwoNumber(num1,num2){
-// //     return (num1+num2); 
-// // }
+// // // function addTwoNumber(num1,num2){
+// // //     return (num1+num2); 
+// // // }
 
-// // const result = addTwoNumber(3,4)
-// // console.log("result is ",result);
+// // // const result = addTwoNumber(3,4)
+// // // console.log("result is ",result);
 
-// function logginUser(username= "Bikku "){
-//     if(username == undefined){
-//         console.log("Enter username");
+// // function logginUser(username= "Bikku "){
+// //     if(username == undefined){
+// //         console.log("Enter username");
         
-//     }
-//     return `${username} Just loggedin`
+// //     }
+// //     return `${username} Just loggedin`
+// // }
+// // const user = logginUser()
+// // console.log(user);
+
+// // rest and spread operator
+// function claculateCartPrices(num01,num02,...num1){
+//     return num1
 // }
-// const user = logginUser()
-// console.log(user);
+// console.log(claculateCartPrices(200,300,400,500));
+
+const user = {
+    username:"Vikash",
+    price:299
+}
+
+function handleObjects(anyobject){
+   console.log(`Username is ${anyobject.username} and prices is ${anyobject.price}`);
+   
+}
+
+handleObjects({
+    username:"Goswami",
+    price:400
+})
