2d27876caff73c97f695b9821c4a39a7945a1f05
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-11 19:30:19 +0000

    functions basics

---
+++ functions/01_functions.js
@@ -0,0 +1,36 @@
+// // // const checkType1 = 3 + "4"
+// // // const checkType2 = "3" + 4
+// // // const checkType3 = "3" + "4"
+// // // const checkType4 = 3 + 4
+
+// // // const checkType5= 3 + "a"
+// // // const checkType6 = "a" + 4
+// // // const checkType7 = "3" + "4"
+// // // const checkType8 = "3a" + 4
+
+// // // console.log(typeof checkType1);
+// // // console.log(typeof checkType2);
+// // // console.log(typeof checkType3);
+// // // console.log(typeof checkType4);
+// // // console.log(typeof checkType5);
+// // // console.log(typeof checkType6);
+// // // console.log(typeof checkType7);
+// // // console.log(typeof checkType8);
+
+
+// // function addTwoNumber(num1,num2){
+// //     return (num1+num2); 
+// // }
+
+// // const result = addTwoNumber(3,4)
+// // console.log("result is ",result);
+
+// function logginUser(username= "Bikku "){
+//     if(username == undefined){
+//         console.log("Enter username");
+        
+//     }
+//     return `${username} Just loggedin`
+// }
+// const user = logginUser()
+// console.log(user);
