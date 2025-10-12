d370214abf86df55c587830f73a05e36e8777b85
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-12 10:34:00 +0000

    understanding the flow for scopes

---
+++ functions/02_scopes.js
@@ -0,0 +1,10 @@
+function one(){
+    const username = "Vikash goswami"
+    function two(){
+        const website = "vikashgoswamiyoutube.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
