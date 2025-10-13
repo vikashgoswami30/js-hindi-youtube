21ef7830cd63a9aafc576ac931056511b5620503
Vikash Goswami <109417405+vikashgoswami30@users.noreply.github.com>
2025-10-13 12:27:43 +0000

    maps and filter for sure basics

--- control_flow/one.js
+++ control_flow/one.js
@@ -1,10 +1,56 @@
-let value1 = 5 ?? 10
+// // // // let value1 = 5 ?? 10
 
-let value2 = null ?? 15
+// // // // let value2 = null ?? 15
 
-let value3 = undefined ?? 100
-console.log(value2);
-console.log(value1);
-console.log(value3);
+// // // // let value3 = undefined ?? 100
+// // // // console.log(value2);
+// // // // console.log(value1);
+// // // // console.log(value3);
 
-console.log(2>6?console.log("Two is greater"):console.log("Two is less"))
+// // // // console.log(2>6?console.log("Two is greater"):console.log("Two is less"))
+
+// // // const arr = [1,2,3,4, "vikash",{username:"Vikasj"}, 5]
+// // // for (const num of arr) {
+// // //     console.log(num);
+// // // }
+
+// // // const str = "Goswami Vikash"
+// // // for (const char of str) {
+// // //     console.log(char);
+    
+// // // }
+
+// // //////////////// MAPS //////////////////
+
+// // const map =  new Map()
+// // map.set('IN', "INDIA")
+// // map.set('USA', "United states of America")
+// // map.set('FR', "France")
+// // map.set('FR', "France")
+
+// // // console.log(map);
+// // for (const [key,value] of map) {
+// //     console.log(key, '+' , value);
+// // }
+
+// // const myObject = {
+// //     'game1' : "NFS",
+// //     'game2' : "GTA",
+// //     'game3' : "SAN ANDREAS"
+// // }
+
+// // for (const [key,values] of myObject) {
+// //     console.log(key, '--' , values); 
+// // } // object cannot iterable
+
+// const objectss = {
+//     js : "JavaScripts",
+//     cpp : "C++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+// }
+
+// for (const key in objectss) {
+//     console.log(key);
+    
+// }
---
+++ control_flow/two.js
@@ -0,0 +1,64 @@
+// // // const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// // // // const values = coding.forEach( (item)=>{
+    //  console.log(item);   
+// // // //     return item
+// // // // })
+
+// // // // console.log(values);
+
+// // // const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// // // const newNums = myNums.filter( (num)=> {
+// //     //     return num > 4
+// //     // })
+// //     // console.log(newNums);
+    
+// // const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// // // const newNums = []
+
+// // // myNums.forEach( (num)=>{
+// // //     if(num > 4){
+// // //         newNums.push(num)
+// // //     }
+// // // } )
+// // // console.log(newNums);
+
+//  const books = [
+//     {
+//         title:'Flamingo', 
+//         genre: "Fiction", 
+//         publish: 1981,
+//         edition:2004
+//     },
+//     {
+//         title:'Gaudaan', 
+//         genre: "History", 
+//         publish: 1982,
+//         edition:2003
+//     },
+//     {
+//         title:'The last Lesson', 
+//         genre: "Fiction", 
+//         publish: 1985,
+//         edition:2006
+//     },
+//     {
+//         title:'Flamingo 2.0', 
+//         genre: "History", 
+//         publish: 2005,
+//         edition:2014
+//     },
+//  ]
+
+//  const bookDetails = books.filter( (bookItem) => bookItem.genre==="History")
+//  const publishedBookAfter2000 = books.filter( (bookPublishedOn)=> {
+//     return bookPublishedOn.publish > 2004 && bookPublishedOn.genre==="History"
+//  })
+
+// //  console.log(bookDetails);
+// console.log(publishedBookAfter2000);
+
+
+ 
